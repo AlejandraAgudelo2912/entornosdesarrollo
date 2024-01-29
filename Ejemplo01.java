8db5ed76553fae73bb6f6c7960709b4628a3b571
daw23-01 <daw23-01@c16x17.iescierva.net>
2024-01-29 19:24:38 +0100

    EJEMPLO01 Añadir nombre al menasaje

--- Ejemplo01.java
+++ Ejemplo01.java
@@ -2,5 +2,7 @@ public class Ejemplo01{
 
     public static void main (String []arvs){
         System.out.println("Hola mundo");
+        String nombre = "david";
+        System.out.println("Mi nombre es: "+nombre);
     }
 }
