25bc4af0f0b62a6508809d15024f932c8878dd46
daw23-01 <daw23-01@c16x17.iescierva.net>
2024-01-31 19:12:29 +0100

    ED46 Añadir bucle for al archivo ejemplo01.java

--- Ejemplo01.java
+++ Ejemplo01.java
@@ -4,5 +4,8 @@ public class Ejemplo01{
         System.out.println("Hola mundo");
         String nombre = "david";
         System.out.println("Mi nombre es -> "+nombre);
+        for (int i =1; i<=10;i++){
+            System.err.println("Vuelta "+(i));
+        }
     }
 }
