90dfa9f19179bd2db00de91d4c3930f19f8a33bd
daw23-01 <daw23-01@c16x17.iescierva.net>
2024-01-31 17:58:20 +0100

    EJEM01 Modificacion del codigo

--- Ejemplo01.java
+++ Ejemplo01.java
@@ -3,6 +3,6 @@ public class Ejemplo01{
     public static void main (String []arvs){
         System.out.println("Hola mundo");
         String nombre = "david";
-        System.out.println("Mi nombre es: "+nombre);
+        System.out.println("Mi nombre es -> "+nombre);
     }
 }
